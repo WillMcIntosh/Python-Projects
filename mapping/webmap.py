a41c06f046e23255fb8a1994e7108e483f3718fc
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-06-19 17:06:33 -0500

    added lamda function for polygon layer based on world population. Also made population layer be underneath volcano markers.

--- mapping/webmap.py
+++ mapping/webmap.py
@@ -18,12 +18,13 @@ map = folium.Map(location=[39.349,-11.86], zoom_start=2, tiles = "Mapbox Bright"
 
 fg = folium.FeatureGroup(name="My Map")
 
+fg.add_child(folium.GeoJson(data=open("/home/will/projects/Python-Projects/mapping/world.json", 'r', encoding='utf-8-sig'), style_function=lambda x: {'fillOpacity':0.7, 'fillColor': 'red' if x['properties'] ['POP2005'] >= 1000000000 else 'orange' if 200000000 <= x['properties'] ['POP2005'] < 10000000000 else 'yellow' if 100000000 <= x['properties'] ['POP2005'] < 200000000 else 'green' if 50000000 <= x['properties'] ['POP2005'] < 100000000 else 'blue' if 25000000 <= x['properties'] ['POP2005'] < 50000000 else 'gray'}))
+
 for lt, ln, nm, el in zip(lat, lon, volcano, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 5, color = colour_producer(el), fill_color = colour_producer(el), fill_opacity=0.8, popup=nm +" , "+ str(el) + " m"))
-
-fg.add_child(folium.GeoJson(data=open("/home/will/projects/Python-Projects/mapping/world.json", 'r', encoding='utf-8-sig'), style_function=lambda x: {'fillColor':'yellow'}))
-
 
 map.add_child(fg)
 
 map.save("Map2.html")
+
+#if x['properties'] ['POP2005'] >= 1000000000 else 'orange' if 200000000 <= x['properties'] ['POP2005'] < 10000000000 else 'yellow' if 100000000 <= x['properties'] ['POP2005'] < 200000000 else 'green' if 50000000 <= x['properties'] ['POP2005'] < 100000000 else 'blue' if 25000000 <= x['properties'] ['POP2005'] < 50000000 else 'gray'

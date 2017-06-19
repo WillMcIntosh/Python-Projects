5f1a00908e8b1d8a6cfd6f694ad6bd853da26dee
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-06-19 16:29:40 -0500

    added polygon layer

--- mapping/webmap.py
+++ mapping/webmap.py
@@ -14,12 +14,14 @@ def colour_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[39.03,-97.60], zoom_start=4, tiles = "Mapbox Bright")
+map = folium.Map(location=[39.349,-11.86], zoom_start=2, tiles = "Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, nm, el in zip(lat, lon, volcano, elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 10, color = colour_producer(el), fill_color = colour_producer(el), fill_opacity=0.8, popup=nm +" , "+ str(el) + " m"))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 5, color = colour_producer(el), fill_color = colour_producer(el), fill_opacity=0.8, popup=nm +" , "+ str(el) + " m"))
+
+fg.add_child(folium.GeoJson(data=open("/home/will/projects/Python-Projects/mapping/world.json", 'r', encoding='utf-8-sig'), style_function=lambda x: {'fillColor':'yellow'}))
 
 
 map.add_child(fg)

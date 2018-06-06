dac065e9d502afde94a28697a3436c62783c4867
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-06-06 12:02:24 -0400

    misc files from previous lectures.

---
+++ Section2-VariablesDataTypes/strings.py
@@ -0,0 +1,16 @@
+c = "Hello there"
+print(c)
+d = c.replace('e','o')
+print(d)
+
+def reversed_string(aString):
+    return aString[::-1]
+
+print(reversed_string(c))
+
+foo = long_function_name(var_one, var_two,
+                        var_three, var_four,
+                        var_five)
+def longer_function_name(
+        var_one, var_two,
+        var_three, var_four)
